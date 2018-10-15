58eb9c137fac84e155020a1bec7c304a23c39656
sheertum <simonvanheertum@gmail.com>
2018-10-15 13:45:55 +0200

    Added commentary to hangar and turnout

--- GoldRush/Hangar.cs
+++ GoldRush/Hangar.cs
@@ -16,6 +16,9 @@ namespace GoldRush
 
         public bool AddCart()
         {
+            /* Eerst checken of er al een cart op de eerste plek staat van deze hangar
+            Zo niet dan kan er een toegevoegd worden.
+            Als dit wel het geval is returnt deze functie false */
             if(this.track.First.occupant != null)
             {
                 return false;
@@ -27,6 +30,10 @@ namespace GoldRush
 
         public void moveCarts()
         {
+            /* Deze functie begint bij de FIRST van het track van de hangar en gaat vervolgens het hele track af
+            totdat er geen tracklink meer is, wat dus kan betekenen dat het aan het einde van het spel is, of dat een TURNOUT van
+            richting is verandert waardoor het track van deze hangar stopt
+            Bij het afgaan van het track word iedere CART op een stack gegooid omdat ze vervolgens in omgekeerde volgorde moeten gaan bewegen om onbedoelde collision te voorkomen */
             Stack<Cart> carts = track.createCartsQueue();
             while(carts.Count > 0)
             {
