ae26f7533cc9969243d2adbab3a2660475c66d9e
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2018-10-16 12:10:01 +0200

    Add Board

---
+++ GoldRush/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldRush
+{
+    public class Board
+    {
+        public int Score { get; private set; }
+        public Quay Quay { get; private set; }
+
+        public TrackLink TrackLinkEnd { get; private set; }
+        public List<Hangar> Hangars { get; private set; }
+        public HashSet<Turnout> Turnouts { get; private set; }
+    }
+}
