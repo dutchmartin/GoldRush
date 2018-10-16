e784b078a8e28eb50c4711d38810c1b645dcf40f
sheertum <simonvanheertum@gmail.com>
2018-10-16 13:13:53 +0200

    Added endgame exception

---
+++ GoldRush/CartCrashException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldRush
+{
+    class CartCrashException : Exception
+    {
+        public String GameOverMessage;
+
+        public CartCrashException()
+        {
+            GameOverMessage = "A cart has crashed, you are gameover";
+        }
+    }
+}
