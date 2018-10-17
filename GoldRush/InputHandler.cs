5f0fb6157455c8c7f1f440135d1f682806b15e95
sheertum <simonvanheertum@gmail.com>
2018-10-17 16:34:02 +0200

    Built inputhandler and mapper

--- GoldRush/InputHandler.cs
+++ GoldRush/InputHandler.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace GoldRush
 {
-    class InputHandler
+    public class InputHandler
     {
+        public ConsoleKey GetKeyInput()
+        {
+            ConsoleKey input;
+            do
+            {
+                input = Console.ReadKey().Key;
+            }
+            while ( ! IsCorrectKey(input));
+            return input;
+        }
+        private Boolean IsCorrectKey(ConsoleKey key)
+        {
+            var KeyCode = (int) key;
+            var min = 49;
+            var max = 53;
+            return TestRange(KeyCode, min, max);
+        }
+        private bool TestRange(int numberToCheck, int bottom, int top)
+        {
+            return (numberToCheck >= bottom && numberToCheck <= top);
+        }
     }
 }
