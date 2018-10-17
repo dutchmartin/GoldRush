2aabc1b7ed4565bec6e842d9126c80f8f927ff4c
sheertum <simonvanheertum@gmail.com>
2018-10-17 13:36:02 +0200

    Added: - AddCart - AddShip - MoveCart - MoveShip

--- GoldRush/Board.cs
+++ GoldRush/Board.cs
@@ -8,20 +8,67 @@ namespace GoldRush
 {
     public class Board
     {
+        private Random random;
         public int Score { get; private set; }
-        public Ship Ship { get; private set; }
+        public River river { get; set; }
+        public List<Ship> Ships { get; private set; }
 
         public Track TrackEnd { get; private set; }
         public List<Hangar> Hangars { get; private set; }
+        public List<Cart> Carts { get; private set; }
         public HashSet<Turnout> Turnouts { get; private set; }
 
-        public Board(int score, Ship ship, Track TrackEnd, List<Hangar> hangars, HashSet<Turnout> turnouts)
+        public Board(int score, Track TrackEnd, List<Hangar> hangars, HashSet<Turnout> turnouts)
         {
+            random = new Random();
             Score = score;
-            Ship = ship;
             TrackEnd = TrackEnd;
+            Carts = new List<Cart>();
             Hangars = hangars;
             Turnouts = turnouts;
+            river = new River();
+            river.Occupant = new Ship(river);
+        }
+
+        public bool AddCarts(int amount)
+        {
+            if(amount > Hangars.Count)
+            {
+                return false;
+            }
+            Hangar ChosenHangar;
+            Cart AddedCart = null;
+            for(int count = 0; count < amount; count++)
+            {
+                while(AddedCart == null)
+                {
+                    ChosenHangar = Hangars[random.Next(1, Hangars.Count)];
+                    AddedCart = ChosenHangar.AddCart();
+                }
+                Carts.Add(AddedCart);
+            }
+            return true;
+        }
+        public bool AddShip()
+        {
+            if(river.Occupant != null)
+            {
+                return false;
+            }
+            if(random.Next(1, 10) != 1)
+            {
+                return false;
+            }
+            river.Occupant = new Ship(river);
+            return true;
+        }
+        public void MoveCarts()
+        {
+            Carts.ForEach(c => c.Move());
+        }
+        public void MoveShips()
+        {
+            Ships.ForEach(s => s.Move());
         }
     }
 }
---
+++ UnitTestProject1/BoardTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using GoldRush;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BoardTests
+    {
+        Board board;
+        Track hangar1Track2;
+        Track hangar2Track2;
+        Track hangar1Track3;
+        [TestInitialize]
+        public void Init()
+        {
+            List<Hangar> hangars = new List<Hangar>();
+            HashSet<Turnout> turnouts = new HashSet<Turnout>();
+            Hangar hangar1 = new Hangar();
+            Hangar hangar2 = new Hangar();
+            hangar1Track2 = new Track();
+            hangar2Track2 = new Track();
+            hangar1Track3 = new Track();
+            Turnout1To2 turnout1 = new Turnout1To2();
+            Turnout2To1 turnout2 = new Turnout2To1();
+
+            hangar1.Next = hangar1Track2;
+            hangar1Track2.Next = hangar1Track3;
+
+            hangar2.Next = hangar2Track2;
+
+            hangars.Add(hangar1);
+            hangars.Add(hangar2);
+
+            turnouts.Add(turnout1);
+            turnouts.Add(turnout2);
+
+            board = new Board(0, null, hangars, turnouts);
+
+        }
+        [TestMethod]
+        public void NewBoardHasEmptyCarts()
+        {
+            Assert.AreEqual(board.Carts.Count, 0);
+        }
+
+        [TestMethod]
+        public void NewBoardHasCorrectAmountOfHangars()
+        {
+            Assert.AreEqual(board.Hangars.Count, 2);
+        }
+        [TestMethod]
+        public void NewBoardHasCorrectAmountOfTurnouts()
+        {
+            Assert.AreEqual(board.Turnouts.Count, 2);
+        }
+        [TestMethod]
+        public void AddCartAddsOneCartToCarts()
+        {
+            board.AddCarts(1);
+            Assert.AreEqual(board.Carts.Count, 1);
+        }
+        [TestMethod]
+        public void AddingMultipleCartsAddsCorrectAmountOfCarts()
+        {
+            board.AddCarts(2);
+            Assert.AreEqual(board.Carts.Count, 2);
+        }
+        [TestMethod]
+        public void AddingTooManyCartsWontWork()
+        {
+            bool result = board.AddCarts(3);
+            Assert.AreEqual(board.Carts.Count, 0);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void AllCartsMove()
+        {
+            board.AddCarts(2);
+            board.MoveCarts();
+            Assert.IsNull(board.Hangars[0].Occupant);
+            Assert.IsNull(board.Hangars[1].Occupant);
+            Assert.IsNotNull(hangar1Track2);
+            Assert.IsNotNull(hangar2Track2);
+        }
+        [TestMethod]
+        public void CartsMoveCorrectly()
+        {
+            board.AddCarts(2);
+            board.MoveCarts();
+            board.MoveCarts();
+            Assert.IsNotNull(hangar1Track3);
+            Assert.IsNotNull(hangar2Track2);
+        }
+        [TestMethod]
+        [ExpectedException (typeof(CartCrashException))]
+        public void CartsCrash()
+        {
+            board.AddCarts(2);
+            board.MoveCarts();
+            board.AddCarts(2);
+            board.MoveCarts();
+        }
+    }
+}
