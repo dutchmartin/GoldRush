96976e0394946c10848b7f858463bd1b2f712741
sheertum <simonvanheertum@gmail.com>
2018-10-17 15:34:24 +0200

    finished testingcase

--- UnitTestProject1/BoardTests.cs
+++ UnitTestProject1/BoardTests.cs
@@ -12,6 +12,9 @@ namespace UnitTestProject1
         Track hangar1Track2;
         Track hangar2Track2;
         Track hangar1Track3;
+        WaterLink river2;
+        WaterQuay quay;
+        WaterLink river4;
         [TestInitialize]
         public void Init()
         {
@@ -24,6 +27,10 @@ namespace UnitTestProject1
             hangar1Track3 = new Track();
             Turnout1To2 turnout1 = new Turnout1To2();
             Turnout2To1 turnout2 = new Turnout2To1();
+            quay = new WaterQuay(hangar1Track2);
+            river2 = new WaterLink();
+            river4 = new WaterLink();
+
 
             hangar1.Next = hangar1Track2;
             hangar1Track2.Next = hangar1Track3;
@@ -37,6 +44,9 @@ namespace UnitTestProject1
             turnouts.Add(turnout2);
 
             board = new Board(0, null, hangars, turnouts);
+            board.FirstRiver.Next = river2;
+            river2.Next = quay;
+            quay.Next = river4;
 
         }
         [TestMethod]
@@ -58,26 +68,26 @@ namespace UnitTestProject1
         [TestMethod]
         public void AddCartAddsOneCartToCarts()
         {
-            board.AddCarts(1);
+            board.HasAddedACart(1);
             Assert.AreEqual(board.Carts.Count, 1);
         }
         [TestMethod]
         public void AddingMultipleCartsAddsCorrectAmountOfCarts()
         {
-            board.AddCarts(2);
+            board.HasAddedACart(2);
             Assert.AreEqual(board.Carts.Count, 2);
         }
         [TestMethod]
         public void AddingTooManyCartsWontWork()
         {
-            bool result = board.AddCarts(3);
+            bool result = board.HasAddedACart(3);
             Assert.AreEqual(board.Carts.Count, 0);
             Assert.IsFalse(result);
         }
         [TestMethod]
         public void AllCartsMove()
         {
-            board.AddCarts(2);
+            board.HasAddedACart(2);
             board.MoveCarts();
             Assert.IsNull(board.Hangars[0].Occupant);
             Assert.IsNull(board.Hangars[1].Occupant);
@@ -87,7 +97,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void CartsMoveCorrectly()
         {
-            board.AddCarts(2);
+            board.HasAddedACart(2);
             board.MoveCarts();
             board.MoveCarts();
             Assert.IsNotNull(hangar1Track3);
@@ -97,10 +107,25 @@ namespace UnitTestProject1
         [ExpectedException (typeof(CartCrashException))]
         public void CartsCrash()
         {
-            board.AddCarts(2);
+            board.HasAddedACart(2);
             board.MoveCarts();
-            board.AddCarts(2);
+            board.HasAddedACart(2);
             board.MoveCarts();
+        }
+        [TestMethod]
+        public void NewBoardHasShip()
+        {
+            Assert.IsNotNull(board.FirstRiver.Occupant);
+        }
+        [TestMethod]
+        public void AddNewShip()
+        {
+            board.MoveShips();
+            Assert.IsNull(board.FirstRiver.Occupant);
+            if(board.HasAddedAShip())
+            {
+                Assert.IsNotNull(river2.Occupant);
+            }
         }
     }
 }
