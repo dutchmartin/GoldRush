97095372ce93b4f4aa9998540dd22df77c147967
sheertum <simonvanheertum@gmail.com>
2018-10-17 13:34:10 +0200

    - Removed Constructors in Turnout to match current game field builder

--- GoldRush/Turnout1to2.cs
+++ GoldRush/Turnout1to2.cs
@@ -5,27 +5,12 @@ using System.Text;
 
 namespace GoldRush
 {
-    public class Turnout1to2:Turnout
+    public class Turnout1To2:Turnout
     {
         /* Voor de duidelijkheid:
         Omdat 1to2 één enkele previous heeft kan deze ook meteen meegegeven worden in de constructor
         Dit maakt het onderscheid tussen 1to2 en 2to1 duidelijker
          */
-        public Turnout1to2(Turnout entry, bool isGoingUp){
-            this.isGoingUp = isGoingUp;
-            previous = entry;
-            optionDown = new Track();
-            optionUp = new Track();
-            if(isGoingUp)
-            {
-                Next = optionUp;
-            }
-            else
-            {
-                Next = optionDown;
-            }
-            //TODO set next!
-        }
 
         public void ChangeDirection()
         {
